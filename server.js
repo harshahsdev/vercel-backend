8b4f3a94300c6e911d9d73d8608c4329ec8fbb2c
harshahsdev <harsha.dev2025@gmail.com>
2026-05-07 14:59:15 +0530

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "vercel-frontend-fvj85hn46-harshahsdevs-projects.vercel.app",
     credentials: true
 }));
 
