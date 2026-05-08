94e852e51b28e06d8355ee4de7e5e4a3bac345ab
harshahsdev <harsha.dev2025@gmail.com>
2026-05-08 20:29:58 +0530

    cors

--- server.js
+++ server.js
@@ -43,6 +43,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options(/.*/, cors(corsOptions));
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && (/\.vercel\.app$/i.test(origin) || allowedOrigins.includes(origin))) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  }
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
