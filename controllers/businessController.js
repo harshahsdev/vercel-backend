487891fba2b9a68e74d41bade028ce2f495acf1a
harshahsdev <harsha.dev2025@gmail.com>
2026-05-08 19:35:46 +0530

    cors

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -40,7 +40,7 @@ export const createBusiness = async (req, res) => {
     }
 
     // duplicate check
-    const existingBusiness = await Business.findOne({ name, location });
+    const existingBusiness = await Business.findOne({ name, location, owner: req.user.userID });
     if (existingBusiness) {
       return res.status(400).json({
         message: "Business with same name and location already exists"
